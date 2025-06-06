9f67dd9eeddf6f7306d16b7196b431f3868554ea
sujeett-singh <sujeetsingh0029@gmail.com>
2025-06-06 16:34:16 +0530

    first commit

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -9,47 +9,52 @@ import UserPrivate from "./UserPrivate";
 import AdminPrivate from "./AdminPrivate";
 import EditUser from "../pages/EditUser";
 
-export let myRoutes = createBrowserRouter([
+export let myRoutes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/signup",
+          element: <Signup />,
+        },
+        {
+          path: "/profile",
+          element: (
+            <UserPrivate>
+              <Profile />
+            </UserPrivate>
+          ),
+        },
+        {
+          path: "/admin",
+          element: (
+            <AdminPrivate>
+              <AdminDashboard />
+            </AdminPrivate>
+          ),
+        },
+        {
+          path: "/edit/:id",
+          element: (
+            <AdminPrivate>
+              <EditUser />
+            </AdminPrivate>
+          ),
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
-      {
-        path: "/profile",
-        element: (
-          <UserPrivate>
-            <Profile />
-          </UserPrivate>
-        ),
-      },
-      {
-        path: "/admin",
-        element: (
-          <AdminPrivate>
-            <AdminDashboard />
-          </AdminPrivate>
-        ),
-      },
-      {
-        path: "/edit/:id",// dynamic routes
-        element: (
-          <AdminPrivate>
-            <EditUser />
-          </AdminPrivate>
-        ),
-      },
-    ],
-  },
-]);
+    basename: "/User-Hive", // 👈 Add this to match your GitHub repo name exactly
+  }
+);
